f03d23c797f0fc90aa02bb5f3a81985fcfe6f97b
gchure <griffinchure@gmail.com>
2024-04-23 23:55:26 -0700

    Added new functions for processing of bioreactor data

--- software/dev/annotation.py
+++ software/dev/annotation.py
@@ -1,4 +1,5 @@
 #%%
+
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
@@ -9,7 +10,6 @@ import scipy.signal
 import scipy.stats
 importlib.reload(diaux.data)
 cor, pal = diaux.viz.matplotlib_style()
-
 data1 = pd.read_csv('./data/2024-04-22_complete_growth_curves.csv')
 data2 = pd.read_csv('./data/2024-04-17_complete_growth_curves.csv')
 data2 = data2[data2['tech_rep'] != 3]
@@ -19,115 +19,37 @@ data =  pd.concat([data1, data2], sort=False)
 data['idx'] = data.groupby(['date', 'tech_rep']).ngroup()
 data = data[(data['od650nm'] >= 0.1) &  (data['od650nm'] <= 1.1)]
 
+
+
+#%%
+
+
 corr_dfs = []
 for g, d in data.groupby('idx'):
     _corr_df = diaux.data.compute_pearson_correlation(d)
+                                                      
     corr_dfs.append(_corr_df)
 corr_df = pd.concat(corr_dfs, sort=False)
 
-_corr_df = corr_df[corr_df['idx'] == 3]
-diaux.data.classify_diauxic_phases(_corr_df)
+_corr_df = corr_df[corr_df['idx'] == 4]
 
 #%%
-# Step 1: savgol filter
-dfs = []
-for g, d in data.groupby('idx'):
-    d = d.copy()
-    d['filtered'] = scipy.signal.savgol_filter(np.log(d['od650nm']),
-                                               window_length=30,
-                                               polyorder=1)
-    dfs.append(d)
-filt_data = pd.concat(dfs, sort=False)
 
-fig, ax = plt.subplots(2, 3, figsize=(6, 4), sharex=True)
-ax = ax.ravel()
-for a in ax:
-    a.set_xlabel('time [hr]', fontsize=6)
-    a.set_ylabel('log optical density', fontsize=6)
-for g, d in data.groupby('idx'):
-    ax[g].plot(d['time_hr'], np.log(d['od650nm']), '.', ms=3, markeredgewidth=0)
-    ax[g].set_title(f'replicate {g}', fontsize=6)
-
-for g, d in filt_data.groupby('idx'):
-    ax[g].plot(d['time_hr'], d['filtered'], 'r-', lw=1)
-ax[-1].axis(False)
+fig, ax = plt.subplots(1,1, figsize=(3, 2))
+ax.plot(_corr_df['time_hr'], _corr_df['pearson_correlation'], lw=1)
 
 
 #%%
-# Compute the pearson correlation coefficient with a length 30 window. 
-dfs = []
-pearson_window = 30
-for g, d in filt_data.groupby('idx'):
-    corr = np.zeros(len(d) - pearson_window)
-    for i in range(len(corr)):
-        corr[i] = scipy.stats.pearsonr(d['time_hr'].values[i:i+pearson_window], 
-                                       d['filtered'].values[i:i+pearson_window])[0]
-    d = d[:-pearson_window].copy()
-    d['pearson_r'] = corr
-    dfs.append(d)
-corr_df = pd.concat(dfs, sort=False)
 
-fig, ax = plt.subplots(2, 3, figsize=(6,4), sharex=True)
-ax = ax.ravel()
-for a in ax:
-    a.set_xlabel('time [hr]', fontsize=6)
-    a.set_ylabel('Pearson correlation coeff.', fontsize=6)
-for g, d in corr_df.groupby('idx'):
-    zero_crossings = np.where(np.diff(np.sign(d['pearson_r'].values)) != 0)[0]
-    # ax[g].fill_betweenx([-1, 1], d['time_hr'].values[zero_crossings[0]],
-                        # d['time_hr'].values[zero_crossings[1]], 
-                        # alpha=0.5, color='k')
-    ax[g].plot(d['time_hr'], d['pearson_r'], 'k-')
-    ax[g].plot(d['time_hr'], np.sign(d['pearson_r']), 'r-')
-ax[-1].axis(False)
-# for a in ax:
-    # a.set_yscale('log')
+lab = diaux.data.classify_diauxic_phases(_corr_df, exponential_pearson_thresh=0.95,
+                                         stall_pearson_thresh=0.8)
 
+
+for g, d in lab.groupby('phase'):
+    plt.plot(d['time_hr_centered'], d['od650nm_centered'], 'o', ms=3, markeredgewidth=0,
+             label=g)
+plt.legend()
+# plt.plot(_corr_df['time_hr'], _corr_df['pearson_correlation'])
 #%%
-# Label exponential regions (corr > exp_thresh) and stalled regions based on 
-# zero crossings
-exp_thresh = 0.995
-stall_thresh = 0.5
-dfs = []
-for g, d in corr_df.groupby('idx'):
-    d = d.copy()
-    zero_crossings = np.where(np.diff(np.sign(d['pearson_r'].values)) != 0)[0]
-    t_start = d['time_hr'].values[zero_crossings[0]-1]
-    t_stop = d['time_hr'].values[zero_crossings[1]-1]
-    d['label'] = 'transition' 
-    d.loc[(d['pearson_r'] >= exp_thresh) & (d['time_hr'].values < t_start),
-          'label'] = 'preshift exponential'
-    d.loc[(d['pearson_r'] >= exp_thresh) & (d['time_hr'].values > t_stop),
-          'label'] = 'postshift exponential'
-    d.loc[(d['time_hr'].values >= t_start) & 
-          (d['time_hr'].values <= t_stop), 'label'] = 'stall'
-    dfs.append(d)
-label_df = pd.concat(dfs, sort=False)
 
-fig, ax = plt.subplots(2, 3, figsize=(6, 4))
-ax = ax.ravel()
-for a in ax:
-    a.set_xlabel('time [hr]', fontsize=6)
-    a.set_ylabel('Pearson correlation coeff.', fontsize=6)
-
-for g, d in label_df.groupby(['idx', 'label']):
-    ax[g[0]].plot(d['time_hr'], np.log(d['od650nm']), '.', ms=3, markeredgewidth=0,
-                  label=g[1])
-
-ax[-1].axis(False)
-ax[0].legend()
-
-#%%
-filt_data = pd.concat(dfs, sort=False)
-
-fig, ax = plt.subplots(2, 3, figsize=(6, 4), sharex=True)
-ax = ax.ravel()
-for a in ax:
-    a.set_xlabel('time [hr]', fontsize=6)
-    a.set_ylabel('inst. growth rate [hr$^{-1}$]', fontsize=6)
-
-for g, d in filt_data.groupby('idx'):
-    ax[g].plot(d['time_hr'].values[1:], np.diff(d['filtered'].values)/np.diff(d['time_hr'].values), 'r-', lw=1)
-ax[-1].axis(False)
-
-plt.tight_layout()
+lag_df = diaux.data.quantify_lag_time(lab)
--- software/diaux/data.py
+++ software/diaux/data.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 from scipy.signal import savgol_filter as scipy_savgol
 from scipy.stats import pearsonr as scipy_pearsonr
+from scipy.stats import linregress as scipy_linregress
 
 def compute_pearson_correlation(data, 
                                 time_col='time_hr',
@@ -70,8 +71,10 @@ def compute_pearson_correlation(data,
 def classify_diauxic_phases(data, 
                           time_col='time_hr',
                           corr_col='pearson_correlation',
-                          exponential_pearson_thresh=0.98,
-                          stall_pearson_thresh=0.75,
+                          od_col = 'od650nm',
+                          center = True,
+                          exponential_pearson_thresh=0.99,
+                          stall_pearson_thresh=0,
                           trim=True,
                           minimum_exponential_length=30,
                           ):
@@ -88,6 +91,12 @@ def classify_diauxic_phases(data,
     corr_col : str
         The column name of the pearson correlation coefficient in the DataFrame.  
         Default is `pearson_correlation.`
+    od_col : str
+        The column name of the optical density in the DataFrame.  This is only 
+        used if `center` is `True. 
+    center : bool
+        If True, the DataFrame will be centered in time and optical density to
+        the onset of the shift.
     exponential_pearson_thresh: float, [-1, 1]
         The threshold above which the Pearson correlation of the log optical 
         density is considered to be linear, and therefore in expoenential
@@ -110,37 +119,109 @@ def classify_diauxic_phases(data,
        The supplied data with the various phases labeled. Labels include `preshift_exponential`,
        `postshift_exponential`, `stall`, and `transition`.  
     """
-
+    data = data.copy(deep=True)
     # Unpack the correlation to a vector to avoid pandas slowness
     corr = data[corr_col].values
+    time = data[time_col].values
 
     # Apply the thresholds
     stall = corr <= stall_pearson_thresh
     exp = corr >= exponential_pearson_thresh   
-    exp_switchpoints = np.where(np.diff(np.sign(exp.astype(float))) != 0)[0]
+    _exp_switchpoints = np.where(np.diff(np.sign(exp.astype(float))) != 0)[0]
     stall_switchpoints = np.where(np.diff(np.sign(stall.astype(float))) != 0)[0]
 
-
     # Pad the exponential switchpoints
-    exp_switchpoints = np.insert(exp_switchpoints, 0, 0)
-    exp_switchpoints = np.append(exp_switchpoints, len(corr)-1)     
+    if _exp_switchpoints[0] != 0:
+        _exp_switchpoints = np.insert(_exp_switchpoints, 0, 0)
+    _exp_switchpoints = np.append(_exp_switchpoints, len(corr)-1)     
 
-    # Compute the phase lengths
-    lengths = np.diff(exp_switchpoints)
-
-    # Determine if the data begins in the exponential phase
-    exp_phase = exp[0]
-
-    _x = [0]
+    # Compute the phase lengths and fold switchpoints that don't meet the 
+    # criteria
+    lengths = np.diff(_exp_switchpoints)
+    exp_switchpoints = [_exp_switchpoints[0]]
     for i, ell in enumerate(lengths):
         if ell > minimum_exponential_length:
-            _x.append(exp_switchpoints[i+1])
-        
-    print(exp_switchpoints, lengths, _x)
+            exp_switchpoints.append(_exp_switchpoints[i+1])
+
+    # Assign the default phases. 
+    data['phase'] = 'transition'
+
+    # Assign the stall phase    
+    stall_start = time[stall_switchpoints[0]-1]
+    stall_stop = time[stall_switchpoints[-1]-1]
+
+    data.loc[(data[time_col] >= stall_start) & 
+             (data[time_col] <= stall_stop), 'phase'] = 'stall'
+
+    intervals = {}
+    if exp[0]:
+        intervals['preshift']  = [time[exp_switchpoints[0]], time[exp_switchpoints[1]]] 
+    else:
+        intervals['preshift']  = [time[exp_switchpoints[1]], time[exp_switchpoints[2]]] 
+    if exp[1]:
+        intervals['postshift'] = [time[exp_switchpoints[-2]] + 1, time[exp_switchpoints][-1]]
+    else: 
+        intervals['postshift'] = [time[exp_switchpoints[-2]] + 1, time[exp_switchpoints][-1]]
+    for k, v in intervals.items():
+        data.loc[(time >= v[0]) & (time <= v[1]), 'phase'] = f'{k}_exponential'
+
+    if trim:
+        data = data[(data[time_col] >= intervals['preshift'][0]) & 
+                    (data[time_col] <= intervals['postshift'][1])]
+
+    # Add centered columns if desired
+    if center:
+        stall_time_init = data[data['phase']=='stall'][time_col].values[0]
+        stall_od_init = data[data['phase']=='stall'][od_col].values[0]
+        data[time_col +'_centered'] = data[time_col] - stall_time_init
+        data[od_col + '_centered'] = data[od_col].values / stall_od_init 
+        if f'log_{od_col}_filtered' in data.keys():
+            data[f'log_{od_col}_filtered_centered'] = data[f'log_{od_col}_filtered'].values / data[data[time_col + '_centered']==0][f'log_{od_col}_filtered'].values[0]
+    return data
+
+def quantify_lag_time(data,
+                      time_col='time_hr',
+                      od_col='od650nm'):
+    """
+    Calculates the heuristic lag time from a labeled diauxic growth curve.
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        A Pandas DataFrame containing the processed and labeled growth curve.
+    time_col: str
+        The column name of the time dimension in the DataFrame. Default is `time_hr.`
+    od_col: str
+        The column name of the optical density in the DataFrame. Default is `od650nm.`
+
+    Returns
+    -------
+    lag_time : pandas.DataFrame
+        A pandas DataFrame reporting the lag time, along with the average stall 
+        biomass and the preshift/postshift exponential growth rates.
+    """
+
+    # Fit the exponential growth rates for each exponential phase
+    preshift = data[data['phase']=='preshift_exponential']
+    postshift = data[data['phase']=='postshift_exponential']
+    preshift_popt = scipy_linregress(preshift[time_col]-preshift[time_col].min(),
+                                   np.log(preshift[od_col]))
+    postshift_popt = scipy_linregress(postshift[time_col]-postshift[time_col].min(),
+                                   np.log(postshift[od_col]))
+
+    # Extract the stall biomass and the postshift initial biomass
+    mean_stall_biomass = data[data['phase']=='stall'][od_col].mean()
 
 
+    # Compute the heuristic lag time
+    t_regrowth = postshift['time_hr'].values[0] + (np.log(mean_stall_biomass) - postshift_popt[1])/postshift_popt[0]
+    t_stall = data[data['phase']=='stall'][time_col].values[0]
+    t_lag = t_regrowth - t_stall
 
-
-
-
-
+    _df = pd.DataFrame({'preshift_growth_rate':preshift_popt[0],
+                        'postshift_growth_rate':postshift_popt[0],
+                        'entry_time':t_stall,
+                        'exit_time':t_regrowth,
+                        'lag_time':t_lag},
+                        index=[0])
+    return _df
